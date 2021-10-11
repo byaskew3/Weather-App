c6f601442dd2f4792d7146a34572a491414f8e11
byaskew3 <byaskew3@gmail.com>
2021-10-10 20:29:23 -0600

    fixed decimals points on degrees

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ let weather = {
         document.querySelector(".icon").src = 
         "https://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".temp").innerText = ((temp*1.8)+ 32).toFixed(2) + "°F";
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/r"
         document.querySelector(".weather").classList.remove("loading");
